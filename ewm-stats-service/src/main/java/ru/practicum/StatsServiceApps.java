d9359f2172c2f5a2b51fb4ac1dfaf86c08d7dae3
Nikolay <Novizdarialokin@yandex.ru>
2024-06-14 11:19:57 +0600

    feat: Добавил main класс StatsServiceApps для модуля ewm-stats-service

---
+++ ewm-stats-service/src/main/java/ru/practicum/StatsServiceApps.java
@@ -0,0 +1,15 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 14.06.2024
+ */
+@SpringBootApplication
+public class StatsServiceApps {
+    public static void main(String[] args) {
+        SpringApplication.run(StatsServiceApps.class,args);
+    }
+}
