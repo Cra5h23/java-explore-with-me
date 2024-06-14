a2e0b934410476dd55a7881875bc1fc7ae69273f
Nikolay <Novizdarialokin@yandex.ru>
2024-06-14 11:47:40 +0600

    feat: Добавил StatsController, И реализовал методы для эндпоинтов POST /hit, GET /stats

---
+++ ewm-stats-service/src/main/java/ru/practicum/controller/StatsController.java
@@ -0,0 +1,47 @@
+package ru.practicum.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.dto.RequestStatsDto;
+import ru.practicum.service.StatsService;
+
+import java.util.List;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 14.06.2024
+ */
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class StatsController {
+    private StatsService statsService;
+
+    @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addStats(@RequestBody RequestStatsDto requestStatsDto) {
+        log.info("POST /hit body={}", requestStatsDto);
+        statsService.addStats(requestStatsDto);
+    }
+
+    @GetMapping("/stats")
+    public ResponseEntity<Object> getStats(@RequestParam String start,
+                                           @RequestParam String end,
+                                           @RequestParam(required = false) List<String> uris,
+                                           @RequestParam(required = false, defaultValue = "false") boolean unique) {
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(statsService.getStats(StatsService.Params.builder()
+                        .start(start)
+                        .end(end)
+                        .unique(unique)
+                        .uris(uris)
+                        .build()));
+    }
+}
