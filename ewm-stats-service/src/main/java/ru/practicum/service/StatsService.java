bdb691315b17f303d706e39ea0cd58e3db9cfebc
Nikolay <Novizdarialokin@yandex.ru>
2024-06-14 11:48:19 +0600

    feat: Добавил interface StatsService и методы addStats, getStats

---
+++ ewm-stats-service/src/main/java/ru/practicum/service/StatsService.java
@@ -0,0 +1,32 @@
+package ru.practicum.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.dto.RequestStatsDto;
+import ru.practicum.dto.ResponseStatsDto;
+
+import java.util.List;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 14.06.2024
+ */
+public interface StatsService {
+    void addStats(RequestStatsDto requestStatsDto);
+
+    List<ResponseStatsDto> getStats(Params params);
+
+    @Builder
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    class Params {
+        private String start;
+        private String end;
+        private List<String> uris;
+        private boolean unique;
+    }
+}
+
