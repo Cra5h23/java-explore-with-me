76e8be843a36fcef88e4fa663febfbbf0a546257
Nikolay <Novizdarialokin@yandex.ru>
2024-06-14 11:46:12 +0600

    feat: Реализовал ResponseStatsDto

--- stats-dto/src/main/java/ru/practicum/dto/ResponseStatsDto.java
+++ stats-dto/src/main/java/ru/practicum/dto/ResponseStatsDto.java
@@ -1,8 +1,20 @@
 package ru.practicum.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author Nikolay Radzivon
  * @Date 14.06.2024
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class ResponseStatsDto {
+    private String app;
+    private String uri;
+    private long hits;
 }
