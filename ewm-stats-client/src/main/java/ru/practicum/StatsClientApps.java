f0ecf3f7ba99203c0a695b18ddfb767e53c88178
Nikolay <Novizdarialokin@yandex.ru>
2024-06-15 00:23:52 +0600

    feat: Добавил main класс StatsClientApps для модуля ewn-stats-client

---
+++ ewm-stats-client/src/main/java/ru/practicum/StatsClientApps.java
@@ -0,0 +1,15 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 14.06.2024
+ */
+@SpringBootApplication
+public class StatsClientApps {
+    public static void main(String[] args) {
+        SpringApplication.run(StatsClientApps.class, args);
+    }
+}
