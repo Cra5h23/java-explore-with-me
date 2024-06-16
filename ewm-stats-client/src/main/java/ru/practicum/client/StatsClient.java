57777cea34c683f530058d512173cb04c779cff7
Nikolay <Novizdarialokin@yandex.ru>
2024-06-16 14:26:48 +0600

    feat: добавил StatsClient

---
+++ ewm-stats-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -0,0 +1,12 @@
+package ru.practicum.client;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 15.06.2024
+ */
+@Component
+public class StatsClient extends RestTemplate {
+}
