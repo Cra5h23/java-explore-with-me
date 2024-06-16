b03240244d10763679f73b6fa6ad2e4253c2fb9d
Nikolay <Novizdarialokin@yandex.ru>
2024-06-16 14:29:29 +0600

    feat: поле statsService теперь final

--- ewm-stats-service/src/main/java/ru/practicum/controller/StatsController.java
+++ ewm-stats-service/src/main/java/ru/practicum/controller/StatsController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 public class StatsController {
-    private StatsService statsService;
+    private final StatsService statsService;
 
     @PostMapping("/hit")
     @ResponseStatus(HttpStatus.CREATED)
