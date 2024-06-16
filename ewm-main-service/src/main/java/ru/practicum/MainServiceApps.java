16e1e85f1abb1e4b89d6ba26e427a9f169a7133f
Nikolay <Novizdarialokin@yandex.ru>
2024-06-16 15:06:29 +0600

    feat: добавил main класс MainServiceApps для модуля ewm-main-service

---
+++ ewm-main-service/src/main/java/ru/practicum/MainServiceApps.java
@@ -0,0 +1,15 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 15.06.2024
+ */
+@SpringBootApplication
+public class MainServiceApps {
+    public static void main(String[] args) {
+        SpringApplication.run(MainServiceApps.class, args);
+    }
+}
