b7c72782f6ed794be4b88ebf480e726c20f34b1c
Nikolay <Novizdarialokin@yandex.ru>
2024-06-16 14:33:27 +0600

    feat: StatsRepository наследует JpaRepository

--- ewm-stats-service/src/main/java/ru/practicum/repository/StatsRepository.java
+++ ewm-stats-service/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -1,8 +1,11 @@
 package ru.practicum.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.practicum.model.Stats;
 /**
  * @author Nikolay Radzivon
  * @Date 14.06.2024
  */
 public interface StatsRepository {
 }
+public interface StatsRepository extends JpaRepository<Stats, Long> {
