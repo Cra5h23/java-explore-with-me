db66ecc2c1d08ce5b8db64d062de5788c06326b1
Nikolay <Novizdarialokin@yandex.ru>
2024-06-16 14:34:39 +0600

    feat: добавил кастомный метод getCount

--- ewm-stats-service/src/main/java/ru/practicum/repository/StatsRepository.java
+++ ewm-stats-service/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -19,3 +19,12 @@ public interface StatsRepository extends JpaRepository<Stats, Long> {
             "where s.timestamp between ?1 and ?2 " +
             "group by s.app , s.uri ", nativeQuery = true)
     List<CountStats> getCountDistinct(String start, String end);
+
+    @Query(value = "select " +
+            "count (s.ip), " +
+            "s.app, " +
+            "s.uri " +
+            "from stats as s " +
+            "where s.timestamp between ?1 and ?2 " +
+            "group by s.app, s.uri ", nativeQuery = true)
+    List<CountStats> getCount(String start, String end);
