845c4eaee378f3c5d871eb1471028d891cb97eef
Nikolay <Novizdarialokin@yandex.ru>
2024-06-16 14:34:17 +0600

    feat: добавил кастомный метод getCountDistinct

--- ewm-stats-service/src/main/java/ru/practicum/repository/StatsRepository.java
+++ ewm-stats-service/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -1,7 +1,9 @@
 package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.model.Stats;
+import java.util.List;
 /**
  * @author Nikolay Radzivon
  * @Date 14.06.2024
@@ -9,3 +11,11 @@ import ru.practicum.model.Stats;
 public interface StatsRepository {
 }
 public interface StatsRepository extends JpaRepository<Stats, Long> {
+    @Query(value = "select " +
+            "count(distinct s.ip), " +
+            "s.app, " +
+            "s.uri " +
+            "from stats as s " +
+            "where s.timestamp between ?1 and ?2 " +
+            "group by s.app , s.uri ", nativeQuery = true)
+    List<CountStats> getCountDistinct(String start, String end);
