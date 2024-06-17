6bc0d9278951b3a69842494f26c9977400027867
Nikolay <Novizdarialokin@yandex.ru>
2024-06-17 16:38:47 +0600

    fix: Сработало?

--- ewm-stats-service/stats-service/src/main/java/ru/practicum/controller/StatsController.java
+++ ewm-stats-service/stats-service/src/main/java/ru/practicum/controller/StatsController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import ru.practicum.dto.RequestStatsDto;
 import ru.practicum.service.StatsService;
 
