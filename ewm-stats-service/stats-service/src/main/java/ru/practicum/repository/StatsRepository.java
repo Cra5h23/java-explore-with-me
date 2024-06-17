205b505d9bb557ff37c0de67084f17c496735986
Nikolay <Novizdarialokin@yandex.ru>
2024-06-17 22:41:43 +0600

    feat: Добавил новые Методы Jpa вместо нативных

--- ewm-stats-service/stats-service/src/main/java/ru/practicum/repository/StatsRepository.java
+++ ewm-stats-service/stats-service/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -2,9 +2,10 @@ package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.model.Stats;
 
-import java.net.URI;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 /**
@@ -12,43 +13,43 @@ import java.util.List;
  * @Date 14.06.2024
  */
 public interface StatsRepository extends JpaRepository<Stats, Long> {
-    @Query(value = "select " +
-            "count(distinct s.ip), " +
-            "s.app, " +
-            "s.uri " +
-            "from stats as s " +
-            "where s.timestamp between ?1 and ?2 " +
-            "group by s.app , s.uri ", nativeQuery = true)
-    List<CountStats> getCountDistinct(String start, String end);
-
-    @Query(value = "select " +
-            "count (s.ip), " +
-            "s.app, " +
-            "s.uri " +
-            "from stats as s " +
-            "where s.timestamp between ?1 and ?2 " +
-            "group by s.app, s.uri ", nativeQuery = true)
-    List<CountStats> getCount(String start, String end);
-
-    @Query(value = "select " +
-            "count (s.ip), " +
-            "s.app, " +
-            "s.uri " +
-            "from stats as s " +
-            "where (s.timestamp between ?1 and ?2) " +
-            "and s.uri in (?3)" +
-            "group by s.app, s.uri ", nativeQuery = true)
-    List<CountStats> getCountByUris(String start, String end, List<URI> uris);
-
-    @Query(value = "select " +
-            "count(distinct s.ip), " +
-            "s.app, " +
-            "s.uri " +
-            "from stats as s " +
-            "where (s.timestamp between ?1 and ?2) " +
-            "and s.uri in (?3)" +
-            "group by s.app , s.uri ", nativeQuery = true)
-    List<CountStats> getCountDistinctByUris(String start, String end, List<URI> uris);
+//    @Query(value = "select " +
+//            "count(distinct s.ip), " +
+//            "s.app, " +
+//            "s.uri " +
+//            "from stats as s " +
+//            "where s.timestamp between ?1 and ?2 " +
+//            "group by s.app , s.uri ", nativeQuery = true)
+//    List<CountStats> getCountDistinct(String start, String end);
+//
+//    @Query(value = "select " +
+//            "count (s.ip), " +
+//            "s.app, " +
+//            "s.uri " +
+//            "from stats as s " +
+//            "where s.timestamp between ?1 and ?2 " +
+//            "group by s.app, s.uri ", nativeQuery = true)
+//    List<CountStats> getCount(String start, String end);
+//
+//    @Query(value = "select " +
+//            "count (s.ip), " +
+//            "s.app, " +
+//            "s.uri " +
+//            "from stats as s " +
+//            "where (s.timestamp between ?1 and ?2) " +
+//            "and s.uri in (?3)" +
+//            "group by s.app, s.uri ", nativeQuery = true)
+//    List<CountStats> getCountByUris(String start, String end, List<URI> uris);
+//
+//    @Query(value = "select " +
+//            "count(distinct s.ip), " +
+//            "s.app, " +
+//            "s.uri " +
+//            "from stats as s " +
+//            "where (s.timestamp between ?1 and ?2) " +
+//            "and s.uri in (?3)" +
+//            "group by s.app , s.uri ", nativeQuery = true)
+//    List<CountStats> getCountDistinctByUris(String start, String end, List<URI> uris);
 
     interface CountStats {
         Long getCount();
@@ -57,5 +58,32 @@ public interface StatsRepository extends JpaRepository<Stats, Long> {
 
         String getUri();
     }
+
+    @Query(value = "SELECT count(s.ip) as count, " +
+            "s.app as app, " +
+            "s.uri as uri " +
+            "from Stats as s " +
+            "where ((:uris) IS NULL OR s.uri in :uris) and " +
+            "(s.timestamp between :start and :end) " +
+            "group by s.app, s.uri " +
+            "order by count(s.ip) DESC ")
+    List<CountStats> getCountHits(@Param("start") ZonedDateTime start,
+                                  @Param("end") ZonedDateTime end,
+                                  @Param("uris") List<String> uris
+    );
+
+    @Query(value = "SELECT count(distinct s.ip) as count, " +
+            "s.app as app, " +
+            "s.uri as uri " +
+            "from Stats as s " +
+            "where ((:uris) IS NULL OR s.uri in :uris) and " +
+            "(s.timestamp between :start and :end) " +
+            "group by s.app, s.uri " +
+            "order by count(distinct s.ip) DESC ")
+    List<CountStats> getUniqueCountHits(@Param("start") ZonedDateTime start,
+                                        @Param("end") ZonedDateTime end,
+                                        @Param("uris") List<String> uris
+    );
+
 }
 
