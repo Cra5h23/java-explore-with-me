09087c8b5c9a9bd394d75afab170fcd37cbde037
Nikolay <Novizdarialokin@yandex.ru>
2024-06-18 10:58:54 +0600

    fix: Убрал лишние методы

--- ewm-stats-service/stats-service/src/main/java/ru/practicum/repository/StatsRepository.java
+++ ewm-stats-service/stats-service/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -13,45 +13,6 @@ import java.util.List;
  * @Date 14.06.2024
  */
 public interface StatsRepository extends JpaRepository<Stats, Long> {
-//    @Query(value = "select " +
-//            "count(distinct s.ip), " +
-//            "s.app, " +
-//            "s.uri " +
-//            "from stats as s " +
-//            "where s.timestamp between ?1 and ?2 " +
-//            "group by s.app , s.uri ", nativeQuery = true)
-//    List<CountStats> getCountDistinct(String start, String end);
-//
-//    @Query(value = "select " +
-//            "count (s.ip), " +
-//            "s.app, " +
-//            "s.uri " +
-//            "from stats as s " +
-//            "where s.timestamp between ?1 and ?2 " +
-//            "group by s.app, s.uri ", nativeQuery = true)
-//    List<CountStats> getCount(String start, String end);
-//
-//    @Query(value = "select " +
-//            "count (s.ip), " +
-//            "s.app, " +
-//            "s.uri " +
-//            "from stats as s " +
-//            "where (s.timestamp between ?1 and ?2) " +
-//            "and s.uri in (?3)" +
-//            "group by s.app, s.uri ", nativeQuery = true)
-//    List<CountStats> getCountByUris(String start, String end, List<URI> uris);
-//
-//    @Query(value = "select " +
-//            "count(distinct s.ip), " +
-//            "s.app, " +
-//            "s.uri " +
-//            "from stats as s " +
-//            "where (s.timestamp between ?1 and ?2) " +
-//            "and s.uri in (?3)" +
-//            "group by s.app , s.uri ", nativeQuery = true)
-//    List<CountStats> getCountDistinctByUris(String start, String end, List<URI> uris);
-
-
     @Query(value = "SELECT count(s.ip) as count, " +
             "s.app as app, " +
             "s.uri as uri " +
