bc823867a2bd31fa65ea29c13d573bf206f4b1bd
Nikolay <Novizdarialokin@yandex.ru>
2024-06-20 16:36:37 +0600

    feat: Добавил UserShortDto

---
+++ ewm-main-service/src/main/java/ru/practicum/dto/user/UserShortDto.java
@@ -0,0 +1,28 @@
+package ru.practicum.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Dto пользователя.
+ *
+ * @author Nikolay Radzivon
+ * @Date 19.06.2024
+ */
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Data
+public class UserShortDto {
+    /**
+     * Идентификационный номер пользователя.
+     */
+    private Long id;
+
+    /**
+     * Имя пользователя.
+     */
+    private String name;
+}
