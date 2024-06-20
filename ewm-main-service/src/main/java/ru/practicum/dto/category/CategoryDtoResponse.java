780f07fbe4d9f2f6e1c953c51ca9f0755d9a1bb8
Nikolay <Novizdarialokin@yandex.ru>
2024-06-20 16:32:13 +0600

    feat: Добавил CategoryDtoResponse

---
+++ ewm-main-service/src/main/java/ru/practicum/dto/category/CategoryDtoResponse.java
@@ -0,0 +1,19 @@
+package ru.practicum.dto.category;
+
+/**
+ * Dto категории.
+ *
+ * @author Nikolay Radzivon
+ * @Date 19.06.2024
+ */
+public class CategoryDtoResponse {
+    /**
+     * Идентификационный номер категории.
+     */
+    private Long id;
+
+    /**
+     * Название категории.
+     */
+    private String name;
+}
