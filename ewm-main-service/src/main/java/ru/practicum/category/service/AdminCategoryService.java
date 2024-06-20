8c0efc1e0b83a9f6d8c802a03d8f87b2c028ea07
Nikolay <Novizdarialokin@yandex.ru>
2024-06-20 16:30:29 +0600

    feat: Добавил AdminCategoryService

---
+++ ewm-main-service/src/main/java/ru/practicum/category/service/AdminCategoryService.java
@@ -0,0 +1,16 @@
+package ru.practicum.category.service;
+
+import ru.practicum.dto.category.CategoryDtoRequest;
+import ru.practicum.dto.category.CategoryDtoResponse;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 19.06.2024
+ */
+public interface AdminCategoryService {
+    CategoryDtoResponse addCategory(CategoryDtoRequest category);
+
+    void deleteCategory(Long catId);
+
+    CategoryDtoResponse updateCategory(Long catId, CategoryDtoRequest category);
+}
