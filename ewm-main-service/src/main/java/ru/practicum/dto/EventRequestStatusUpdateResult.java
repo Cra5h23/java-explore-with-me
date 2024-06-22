0a5feb5bb31633de39170af9a36c4cb43b43cdc3
Nikolay <Novizdarialokin@yandex.ru>
2024-06-22 11:32:29 +0600

    feat: Добавил EventRequestStatusUpdateResult

---
+++ ewm-main-service/src/main/java/ru/practicum/dto/EventRequestStatusUpdateResult.java
@@ -0,0 +1,30 @@
+package ru.practicum.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Результат подтверждения/отклонения заявок на участие в событии
+ *
+ * @author Nikolay Radzivon
+ * @Date 22.06.2024
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventRequestStatusUpdateResult {
+    /**
+     * Список подтверждённых заявок.
+     */
+    List<ParticipationRequestDto> confirmedRequests;
+
+    /**
+     * Список отклонённых заявок.
+     */
+    List<ParticipationRequestDto> rejectedRequests;
+}
