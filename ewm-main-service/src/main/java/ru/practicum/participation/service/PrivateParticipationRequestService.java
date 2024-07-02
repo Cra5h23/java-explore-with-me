4e9046025d0bd4fb93cd6fbece0aa0beb7550421
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 18:41:14 +0600

    feat: добавил PrivateParticipationRequestService

---
+++ ewm-main-service/src/main/java/ru/practicum/participation/service/PrivateParticipationRequestService.java
@@ -0,0 +1,17 @@
+package ru.practicum.participation.service;
+
+import ru.practicum.dto.ParticipationRequestDto;
+
+import java.util.List;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 22.06.2024
+ */
+public interface PrivateParticipationRequestService {
+    List<ParticipationRequestDto> getParticipationRequests(Long userId);
+
+    ParticipationRequestDto addParticipationRequest(Long userId, Long eventId);
+
+    ParticipationRequestDto cancelParticipationRequest(Long userId, Long requestId);
+}
