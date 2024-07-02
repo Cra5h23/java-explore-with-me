f702b7377fe502bfb958362a35f513c983686a7a
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 19:30:59 +0600

    feat: добавил ValidDateRange

---
+++ ewm-main-service/src/main/java/ru/practicum/validator/ValidDateRange.java
@@ -0,0 +1,23 @@
+package ru.practicum.validator;
+
+import javax.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 02.07.2024
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = DateRangeValidator.class)
+public @interface ValidDateRange {
+    String message() default "Дата окончания поиска должна быть позже даты начала поиска";
+
+    Class<?>[] groups() default {};
+
+    Class<?>[] payload() default {};
+}
+
