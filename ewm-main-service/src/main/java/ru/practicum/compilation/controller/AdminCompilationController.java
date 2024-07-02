dfce5ba449f6e6725926182bb94b9d7ad1dc2581
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 17:46:38 +0600

    feat: добавил AdminCompilationController

---
+++ ewm-main-service/src/main/java/ru/practicum/compilation/controller/AdminCompilationController.java
@@ -0,0 +1,59 @@
+package ru.practicum.compilation.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.compilation.service.AdminCompilationService;
+import ru.practicum.dto.compilation.NewCompilationDto;
+import ru.practicum.dto.compilation.UpdateCompilationRequest;
+
+import javax.validation.Valid;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 22.06.2024
+ */
+@RestController
+@RequestMapping("/admin/compilations")
+@RequiredArgsConstructor
+@Slf4j
+@Validated
+public class AdminCompilationController {
+    private final AdminCompilationService adminCompilationService;
+
+    @PostMapping
+    public ResponseEntity<Object> addCompilation(@RequestBody @Valid NewCompilationDto compilation) {
+        log.info("POST /admin/compilations");
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(adminCompilationService.addCompilation(compilation));
+
+        //todo 400 Запрос составлен некорректно 409 Нарушение целостности данных
+    }
+
+    @DeleteMapping("/{compId}")
+    public ResponseEntity<Object> deleteCompilation(@PathVariable Long compId) {
+        log.info("DELETE /admin/compilations/{}", compId);
+
+        adminCompilationService.deleteCompilation(compId);
+        return ResponseEntity.noContent().build();
+
+        //todo 404 Подборка не найдена или недоступна
+    }
+
+    @PatchMapping("/{compId}")
+    public ResponseEntity<Object> updateCompilation(@PathVariable Long compId, @RequestBody @Valid UpdateCompilationRequest compilation) {
+        log.info("PATCH /admin/compilations/{} body={}", compId, compilation);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(adminCompilationService.updateCompilation(compId, compilation));
+    }
+}
