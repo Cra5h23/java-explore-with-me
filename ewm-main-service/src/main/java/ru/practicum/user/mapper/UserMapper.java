c8ae35f864636db1865abc8e67ed73f8eca45204
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 19:30:10 +0600

    feat: добавил UserMapper

---
+++ ewm-main-service/src/main/java/ru/practicum/user/mapper/UserMapper.java
@@ -0,0 +1,42 @@
+package ru.practicum.user.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.dto.user.NewUserRequest;
+import ru.practicum.dto.user.UserDtoResponse;
+import ru.practicum.user.model.User;
+
+/**
+ * Маппер для преобразования сущности {@link User} в dto.
+ *
+ * @author Nikolay Radzivon
+ * @Date 23.06.2024
+ */
+@Component
+public class UserMapper {
+    /**
+     * Метод преобразования Dto {@link NewUserRequest} в сущность {@link User}.
+     *
+     * @param user данные пользователя.
+     * @return {@link User} сущность пользователя.
+     */
+    public User toUser(NewUserRequest user) {
+        return User.builder()
+                .email(user.getEmail())
+                .name(user.getName())
+                .build();
+    }
+
+    /**
+     * Метод преобразования сущности {@link User} в Dto {@link UserDtoResponse}.
+     *
+     * @param user данные пользователя.
+     * @return {@link UserDtoResponse} Dto пользователя.
+     */
+    public UserDtoResponse toUserDtoResponse(User user) {
+        return UserDtoResponse.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .name(user.getName())
+                .build();
+    }
+}
