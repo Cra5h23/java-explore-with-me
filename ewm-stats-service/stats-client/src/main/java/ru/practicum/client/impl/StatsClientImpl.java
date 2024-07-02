4355ea851da8b20f52b1a630b4282f1a3f6e3c36
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 21:52:03 +0600

    refactor: Теперь используется LocalDateTime

--- ewm-stats-service/stats-client/src/main/java/ru/practicum/client/impl/StatsClientImpl.java
+++ ewm-stats-service/stats-client/src/main/java/ru/practicum/client/impl/StatsClientImpl.java
@@ -2,11 +2,14 @@ package ru.practicum.client.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.practicum.client.StatsClient;
 import ru.practicum.dto.RequestHitDto;
+import ru.practicum.dto.ResponseStatsDto;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.URLEncoder;
@@ -27,26 +30,43 @@ public class StatsClientImpl extends RestTemplate implements StatsClient {
     @Value("${ewm-stats-service.url}")
     private String ewmStatsServiceUrl;
 
-    public void saveStats(HttpServletRequest request, String appName) {
-        RequestHitDto dto = makeDto(request, appName);
+    @Value("${datetime.format}")
+    private String dateTimeFormat;
+
+    @Override
+    public void saveStats(String ip, String uri, String appName) {
+        var dto = RequestHitDto.builder()
+                .ip(ip)
+                .uri(uri)
+                .app(appName)
+                .timestamp(LocalDateTime.now()
+                        .format(DateTimeFormatter.ofPattern(dateTimeFormat)))
+                .build();
 
         postForLocation(ewmStatsServiceUrl + "/hit", dto);
         log.info("Создан запрос: POST {}/hit body={}", ewmStatsServiceUrl, dto);
     }
 
     @Override
-    public ResponseEntity<Object> getStats(String start, String end, List<String> uris, Boolean unique) {
+    public ResponseEntity<List<ResponseStatsDto>> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        log.info("start = {}, end={}, uris ={}, unique = {}", start, end, uris, unique);
+
         var url = new StringBuilder(ewmStatsServiceUrl + "/stats?");
         Map<String, Object> uriVariables = new HashMap<>();
 
         if (start != null) {
-            var startEncode = URLEncoder.encode(start, StandardCharsets.UTF_8);
+            var startEncode = URLEncoder.encode(start.format(DateTimeFormatter.ofPattern(dateTimeFormat)),
+                    StandardCharsets.UTF_8);
+
             uriVariables.put("start", startEncode);
             url.append("start={start}");
         }
 
         if (end != null) {
-            var endEncode = URLEncoder.encode(end, StandardCharsets.UTF_8);
+//            var endEncode = URLEncoder.encode(end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+//                    StandardCharsets.UTF_8);
+            var endEncode = URLEncoder.encode(end.format(DateTimeFormatter.ofPattern(dateTimeFormat)),
+                    StandardCharsets.UTF_8);
             uriVariables.put("end", endEncode);
             url.append("&end={end}");
         }
@@ -60,9 +80,11 @@ public class StatsClientImpl extends RestTemplate implements StatsClient {
             uriVariables.put("unique", unique);
             url.append("&unique={unique}");
         }
-        log.info("Создан запрос: GET {}", url);
+        log.info("Создан запрос: GET {}/stats?start={}&end={}&uris={}&unique={}", ewmStatsServiceUrl, start, end, uris, unique);
 
-        return getForEntity(url.toString(), Object.class, uriVariables);
+        return exchange(url.toString(), HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<ResponseStatsDto>>() {
+                }, uriVariables);
     }
 
     private RequestHitDto makeDto(HttpServletRequest request, String appName) {
