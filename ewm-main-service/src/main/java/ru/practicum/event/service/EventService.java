8b19ebfebb7b06d49c353dddb411b753270c7b12
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 18:34:48 +0600

    feat: Добавил EventService

---
+++ ewm-main-service/src/main/java/ru/practicum/event/service/EventService.java
@@ -0,0 +1,32 @@
+package ru.practicum.event.service;
+
+import ru.practicum.dto.event.EventShortDto;
+import ru.practicum.event.model.Event;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 26.06.2024
+ */
+public interface EventService {
+    Event checkEvent(Long eventId);
+
+    Event checkEvent(Long eventId, Long initiatorId);
+
+    Map<Long, Long> getViews(LocalDateTime rangeStart, LocalDateTime rangeEnd, List<String> uris);
+
+    void saveStats(HttpServletRequest request, List<String> uris, String appName);
+
+    void saveStats(HttpServletRequest request, String uri, String appName);
+
+    long getViews(LocalDateTime rangeStart, LocalDateTime rangeEnd, String uri);
+
+    List<EventShortDto> getEvents(Collection<Long> events);
+
+    Map<Long, List<EventShortDto>> getEvents(Map<Long, List<Long>> compilationsEvents);
+}
