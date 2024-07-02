120e500e6ac29096088bd49448839cb03d8ebe99
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 17:53:33 +0600

    feat: Добавил Category

---
+++ ewm-main-service/src/main/java/ru/practicum/category/model/Category.java
@@ -0,0 +1,44 @@
+package ru.practicum.category.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 25.06.2024
+ */
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "categories")
+public class Category {
+    /**
+     * Идентификационный номер категории.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Название категории.
+     */
+    @Column(name = "name", length = 50, nullable = false, unique = true)
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        return id != null && id.equals(((Category) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
