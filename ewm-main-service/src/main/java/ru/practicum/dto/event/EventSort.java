e67cdd44d88b82d33136a59d650cd9a5fff5edad
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 18:35:27 +0600

    feat: Добавил EventSort

---
+++ ewm-main-service/src/main/java/ru/practicum/dto/event/EventSort.java
@@ -0,0 +1,18 @@
+package ru.practicum.dto.event;
+
+/**
+ * Список сортировок.
+ *
+ * @author Nikolay Radzivon
+ * @Date 22.06.2024
+ */
+public enum EventSort {
+    /**
+     * Сортировка событий по дате.
+     */
+    EVENT_DATE,
+    /**
+     * Сортировка событий по просмотрам.
+     */
+    VIEWS
+}
