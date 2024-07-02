fc5dee76f4234d4e4f56ac45dbccda83b2514837
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 19:29:21 +0600

    feat: добавил UpdateCompilationRequest

---
+++ ewm-main-service/src/main/java/ru/practicum/dto/compilation/UpdateCompilationRequest.java
@@ -0,0 +1,37 @@
+package ru.practicum.dto.compilation;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+/**
+ * Dto для сущности подборки событий. Используется для получения новых данных о подборке событий для обновления.
+ *
+ * @author Nikolay Radzivon
+ * @Date 23.06.2024
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateCompilationRequest {
+    /**
+     * Список идентификационных номеров событий. Для полной замены предыдущего.
+     */
+    private Set<Long> events;
+
+    /**
+     * Флаг закреплена ли подборка на главной странице сайта.
+     */
+    private Boolean pinned;
+
+    /**
+     * Новый заголовок подборки.
+     */
+    @Size(min = 1, max = 50, message = "Заголовок подборки не может быть меньше {min} и больше {max} символов")
+    private String title;
+}
