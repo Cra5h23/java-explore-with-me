680ce30ae18800ca7bdb19e94b71d0f81c8cc414
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 18:27:10 +0600

    feat: Добавил CustomParticipationRequestRepository

---
+++ ewm-main-service/src/main/java/ru/practicum/participation/repository/CustomParticipationRequestRepository.java
@@ -0,0 +1,13 @@
+package ru.practicum.participation.repository;
+
+import ru.practicum.dto.EventRequestStatus;
+
+import java.util.List;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 28.06.2024
+ */
+public interface CustomParticipationRequestRepository {
+    void updateStatusForRequests(EventRequestStatus status, List<Long> ids, Long eventId, Long initiatorId);
+}
