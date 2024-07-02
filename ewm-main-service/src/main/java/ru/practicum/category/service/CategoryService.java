60be7224b9ba48815ed6172e323f49d62db2c5cd
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 17:54:53 +0600

    feat: Добавил CategoryService

---
+++ ewm-main-service/src/main/java/ru/practicum/category/service/CategoryService.java
@@ -0,0 +1,11 @@
+package ru.practicum.category.service;
+
+import ru.practicum.category.model.Category;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 27.06.2024
+ */
+public interface CategoryService {
+    Category checkCategory(Long catId);
+}
