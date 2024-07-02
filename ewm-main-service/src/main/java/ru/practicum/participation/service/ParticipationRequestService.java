f5fe96c14a5cbc7017d56e7ce7e6aabc0cdf7d2b
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 18:39:19 +0600

    feat: Добавил ParticipationRequestService

---
+++ ewm-main-service/src/main/java/ru/practicum/participation/service/ParticipationRequestService.java
@@ -0,0 +1,24 @@
+package ru.practicum.participation.service;
+
+import ru.practicum.dto.EventRequestStatusUpdateRequest;
+import ru.practicum.dto.EventRequestStatusUpdateResult;
+import ru.practicum.dto.ParticipationRequestDto;
+import ru.practicum.participation.model.ParticipationRequest;
+
+import java.util.List;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 27.06.2024
+ */
+public interface ParticipationRequestService {
+    ParticipationRequest checkParticipationRequest(Long partId);
+
+    ParticipationRequest checkParticipationRequest(Long partId, Long userId);
+
+    List<ParticipationRequestDto> getEventRequests(Long userId, Long eventId);
+
+    EventRequestStatusUpdateResult confirmUserRequests(Long userId, Long eventId, EventRequestStatusUpdateRequest request);
+}
+
+
