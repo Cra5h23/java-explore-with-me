00a690d8c55eccd50c4c59c516fff7b098a5b24c
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 17:56:42 +0600

    feat: Добавил CompilationMapper

---
+++ ewm-main-service/src/main/java/ru/practicum/compilation/mapper/CompilationMapper.java
@@ -0,0 +1,46 @@
+package ru.practicum.compilation.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.compilation.model.Compilation;
+import ru.practicum.dto.compilation.CompilationDtoResponse;
+import ru.practicum.dto.compilation.NewCompilationDto;
+import ru.practicum.dto.event.EventShortDto;
+import ru.practicum.event.model.Event;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 02.07.2024
+ */
+@Component
+public class CompilationMapper {
+
+    public Compilation toCompilation(NewCompilationDto compilation) {
+        return Compilation.builder()
+                .events(compilation.getEvents() != null ? compilation.getEvents().stream()
+                        .map(id -> Event.builder().id(id).build())
+                        .collect(Collectors.toSet()) : null)
+                .pinned(compilation.isPinned())
+                .title(compilation.getTitle())
+                .build();
+    }
+
+    public CompilationDtoResponse toCompilationResponse(Compilation compilation, List<EventShortDto> eventShortDtoList) {
+        return CompilationDtoResponse.builder()
+                .id(compilation.getId())
+                .title(compilation.getTitle())
+                .pinned(compilation.isPinned())
+                .events(eventShortDtoList)
+                .build();
+    }
+
+    public List<CompilationDtoResponse> toListCompilationDtoResponse(List<Compilation> compilations,
+                                                                     Map<Long, List<EventShortDto>> events) {
+        return compilations.stream()
+                .map(c -> toCompilationResponse(c, events.get(c.getId())))
+                .collect(Collectors.toList());
+    }
+}
