5c8fa13f2e888322f99c52cc68cdf4b1c96e1053
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 19:30:48 +0600

    feat: добавил UserServiceImpl

---
+++ ewm-main-service/src/main/java/ru/practicum/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package ru.practicum.user.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.practicum.exception.NotFoundUserException;
+import ru.practicum.user.model.User;
+import ru.practicum.user.repository.UserRepository;
+import ru.practicum.user.service.UserService;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 26.06.2024
+ */
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User checkUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundUserException(
+                        String.format("Пользователь с id %d не существует или не доступен", userId)));
+    }
+}
