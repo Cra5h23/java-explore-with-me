da593fb787d42947f468eecb1d8f0ff4e5b7042e
Nikolay <Novizdarialokin@yandex.ru>
2024-07-02 19:29:57 +0600

    feat: добавил UserDtoResponse

---
+++ ewm-main-service/src/main/java/ru/practicum/dto/user/UserDtoResponse.java
@@ -0,0 +1,33 @@
+package ru.practicum.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Dto для сущности пользователь. Используется в качестве ответа.
+ *
+ * @author Nikolay Radzivon
+ * @Date 22.06.2024
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDtoResponse {
+    /**
+     * Электронная почта пользователя.
+     */
+    private String email;
+
+    /**
+     * Идентификационный номер пользователя.
+     */
+    private Long id;
+
+    /**
+     * Имя пользователя.
+     */
+    private String name;
+}
