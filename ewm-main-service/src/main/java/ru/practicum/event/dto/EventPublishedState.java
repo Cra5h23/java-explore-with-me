3b40514b2cc8d6121df131d8455ed61000fb765a
Nikolay <Novizdarialokin@yandex.ru>
2024-07-04 21:19:46 +0600

    fix: Переименовал EventPublisedState -> EventPublishedState

---
+++ ewm-main-service/src/main/java/ru/practicum/event/dto/EventPublishedState.java
@@ -0,0 +1,17 @@
+package ru.practicum.event.dto;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 27.06.2024
+ */
+public enum EventPublishedState {
+    /**
+     * Опубликовать ивент.
+     */
+    PUBLISH_EVENT,
+
+    /**
+     * Отменить публикацию ивента.
+     */
+    REJECT_EVENT
+}
