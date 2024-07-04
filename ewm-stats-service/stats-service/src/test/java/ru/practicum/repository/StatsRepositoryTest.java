1de9d03f4204f5f674d3ecd1e777a0f4b2dd8d1c
Nikolay <Novizdarialokin@yandex.ru>
2024-07-04 12:43:03 +0600

    fix: Исправление в связи с новым функционалом

--- ewm-stats-service/stats-service/src/test/java/ru/practicum/repository/StatsRepositoryTest.java
+++ ewm-stats-service/stats-service/src/test/java/ru/practicum/repository/StatsRepositoryTest.java
@@ -1,13 +1,19 @@
 package ru.practicum.repository;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import ru.practicum.model.Hit;
+import ru.practicum.service.StatsService;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author Nikolay Radzivon
@@ -65,90 +71,98 @@ class StatsRepositoryTest {
                         .atZone(ZoneId.systemDefault()))
                 .build());
     }
+
+    @Test
+    void getCountHitsTestUrisNull() {
+        var test = statsRepository.getCountHit(
+                StatsService.Params.builder()
+                        .start(LocalDateTime.of(2020, 9, 6, 11, 0, 23))
+                        .end(LocalDateTime.of(2025, 9, 6, 11, 0, 23))
+                        .uris(null)
+                        .unique(false)
+                        .build());
+
+        long count = statsRepository.count();
+        assertEquals(5, count);
+        assertNotNull(test);
+        assertEquals(3, test.size());
+        assertEquals(3, test.get(0).getCount());
+        assertEquals("/events/1", test.get(0).getUri());
+        assertEquals("ewm-main-service", test.get(0).getApp());
+        assertEquals(1, test.get(1).getCount());
+        assertEquals("/events/2", test.get(1).getUri());
+        assertEquals("ewm-main-service", test.get(1).getApp());
+        assertEquals(1, test.get(2).getCount());
+        assertEquals("/events/3", test.get(2).getUri());
+        assertEquals("ewm-main-service", test.get(2).getApp());
+    }
+
+    @Test
+    void getCountHitsTestUrisExists() {
+        var test = statsRepository.getCountHit(
+                StatsService.Params.builder()
+                        .start(LocalDateTime.of(2020, 9, 6, 11, 0, 23))
+                        .end(LocalDateTime.of(2025, 9, 6, 11, 0, 23))
+                        .uris(List.of("/events/1", "/events/2"))
+                        .unique(false)
+                        .build());
+
+        long count = statsRepository.count();
+        assertEquals(5, count);
+        assertNotNull(test);
+        assertEquals(2, test.size());
+        assertEquals(3, test.get(0).getCount());
+        assertEquals("/events/1", test.get(0).getUri());
+        assertEquals("ewm-main-service", test.get(0).getApp());
+        assertEquals(1, test.get(1).getCount());
+        assertEquals("/events/2", test.get(1).getUri());
+        assertEquals("ewm-main-service", test.get(1).getApp());
+    }
+
+    @Test
+    void getUniqueCountHitsUrisNull() {
+        var test = statsRepository.getCountHit(
+                StatsService.Params.builder()
+                        .start(LocalDateTime.of(2020, 9, 6, 11, 0, 23))
+                        .end(LocalDateTime.of(2025, 9, 6, 11, 0, 23))
+                        .uris(null)
+                        .unique(true)
+                        .build());
+
+        long count = statsRepository.count();
+        assertEquals(5, count);
+        assertNotNull(test);
+        assertEquals(3, test.size());
+        assertEquals(2, test.get(0).getCount());
+        assertEquals("/events/1", test.get(0).getUri());
+        assertEquals("ewm-main-service", test.get(0).getApp());
+        assertEquals(1, test.get(1).getCount());
+        assertEquals("/events/2", test.get(1).getUri());
+        assertEquals("ewm-main-service", test.get(1).getApp());
+        assertEquals(1, test.get(2).getCount());
+        assertEquals("/events/3", test.get(2).getUri());
+        assertEquals("ewm-main-service", test.get(2).getApp());
+    }
+
+    @Test
+    void getUniqueCountHitsUrisExists() {
+        var test = statsRepository.getCountHit(
+                StatsService.Params.builder()
+                        .start(LocalDateTime.of(2020, 9, 6, 11, 0, 23))
+                        .end(LocalDateTime.of(2025, 9, 6, 11, 0, 23))
+                        .uris(List.of("/events/1", "/events/2"))
+                        .unique(true)
+                        .build());
+
+        long count = statsRepository.count();
+        assertEquals(5, count);
+        assertNotNull(test);
+        assertEquals(2, test.size());
+        assertEquals(2, test.get(0).getCount());
+        assertEquals("/events/1", test.get(0).getUri());
+        assertEquals("ewm-main-service", test.get(0).getApp());
+        assertEquals(1, test.get(1).getCount());
+        assertEquals("/events/2", test.get(1).getUri());
+        assertEquals("ewm-main-service", test.get(1).getApp());
+    }
 }
-//    @Test
-//    void getCountHitsTestUrisNull() {
-//        var test = statsRepository.getCountHits(LocalDateTime.parse("2020-09-06 11:00:23",
-//                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-//                .atZone(ZoneId.systemDefault()), LocalDateTime.parse("2025-09-06 11:00:23",
-//                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-//                .atZone(ZoneId.systemDefault()), null);
-//
-//        long count = statsRepository.count();
-//        assertEquals(5, count);
-//        assertNotNull(test);
-//        assertEquals(3, test.size());
-//        assertEquals(3, test.get(0).getCount());
-//        assertEquals("/events/1", test.get(0).getUri());
-//        assertEquals("ewm-main-service", test.get(0).getApp());
-//        assertEquals(1, test.get(1).getCount());
-//        assertEquals("/events/2", test.get(1).getUri());
-//        assertEquals("ewm-main-service", test.get(1).getApp());
-//        assertEquals(1, test.get(2).getCount());
-//        assertEquals("/events/3", test.get(2).getUri());
-//        assertEquals("ewm-main-service", test.get(2).getApp());
-//    }
-//
-//    @Test
-//    void getCountHitsTestUrisExists() {
-//        var test = statsRepository.getCountHits(LocalDateTime.parse("2020-09-06 11:00:23",
-//                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-//                .atZone(ZoneId.systemDefault()), LocalDateTime.parse("2025-09-06 11:00:23",
-//                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-//                .atZone(ZoneId.systemDefault()), List.of("/events/1", "/events/2"));
-//
-//        long count = statsRepository.count();
-//        assertEquals(5, count);
-//        assertNotNull(test);
-//        assertEquals(2, test.size());
-//        assertEquals(3, test.get(0).getCount());
-//        assertEquals("/events/1", test.get(0).getUri());
-//        assertEquals("ewm-main-service", test.get(0).getApp());
-//        assertEquals(1, test.get(1).getCount());
-//        assertEquals("/events/2", test.get(1).getUri());
-//        assertEquals("ewm-main-service", test.get(1).getApp());
-//    }
-//
-//    @Test
-//    void getUniqueCountHitsUrisNull() {
-//        var test = statsRepository.getUniqueCountHits(LocalDateTime.parse("2020-09-06 11:00:23",
-//                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-//                .atZone(ZoneId.systemDefault()), LocalDateTime.parse("2025-09-06 11:00:23",
-//                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-//                .atZone(ZoneId.systemDefault()), null);
-//
-//        long count = statsRepository.count();
-//        assertEquals(5, count);
-//        assertNotNull(test);
-//        assertEquals(3, test.size());
-//        assertEquals(2, test.get(0).getCount());
-//        assertEquals("/events/1", test.get(0).getUri());
-//        assertEquals("ewm-main-service", test.get(0).getApp());
-//        assertEquals(1, test.get(1).getCount());
-//        assertEquals("/events/2", test.get(1).getUri());
-//        assertEquals("ewm-main-service", test.get(1).getApp());
-//        assertEquals(1, test.get(2).getCount());
-//        assertEquals("/events/3", test.get(2).getUri());
-//        assertEquals("ewm-main-service", test.get(2).getApp());
-//    }
-//
-//    @Test
-//    void getUniqueCountHitsUrisExists() {
-//        var test = statsRepository.getUniqueCountHits(LocalDateTime.parse("2020-09-06 11:00:23",
-//                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-//                .atZone(ZoneId.systemDefault()), LocalDateTime.parse("2025-09-06 11:00:23",
-//                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-//                .atZone(ZoneId.systemDefault()), List.of("/events/1", "/events/2"));
-//
-//        long count = statsRepository.count();
-//        assertEquals(5, count);
-//        assertNotNull(test);
-//        assertEquals(2, test.size());
-//        assertEquals(2, test.get(0).getCount());
-//        assertEquals("/events/1", test.get(0).getUri());
-//        assertEquals("ewm-main-service", test.get(0).getApp());
-//        assertEquals(1, test.get(1).getCount());
-//        assertEquals("/events/2", test.get(1).getUri());
-//        assertEquals("ewm-main-service", test.get(1).getApp());
-//    }
-//}
