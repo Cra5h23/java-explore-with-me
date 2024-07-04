f6c519affbc5c73532161ce97dc910ebe45dce90
Nikolay <Novizdarialokin@yandex.ru>
2024-07-04 20:52:51 +0600

    refactor: теперь используется класс AdminEventService.GetEventsParams для получения параметров из запроса

--- ewm-main-service/src/main/java/ru/practicum/event/controller/AdminEventController.java
+++ ewm-main-service/src/main/java/ru/practicum/event/controller/AdminEventController.java
@@ -7,15 +7,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.event.dto.EventState;
 import ru.practicum.event.dto.UpdateEventAdminRequest;
 import ru.practicum.event.service.AdminEventService;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * Контроллер для работы с событиями от имени администратора.
@@ -36,29 +31,33 @@ public class AdminEventController {
      */
     @GetMapping
     public ResponseEntity<Object> getEvents(
-            @RequestParam(required = false) List<Long> users,
-            @RequestParam(required = false) List<EventState> states,
-            @RequestParam(required = false) List<Long> categories,
-            @RequestParam(required = false) LocalDateTime rangeStart,
-            @RequestParam(required = false) LocalDateTime rangeEnd,
-            @RequestParam(required = false, defaultValue = "0") @Valid
-            @Min(value = 0, message = "Параметр from не может быть меньше {value}") int from,
-            @RequestParam(required = false, defaultValue = "10")
-            @Min(value = 1, message = "Параметр size не может быть меньше {value}") @Valid
-            @Max(value = 1000, message = "Параметр size не может быть больше {value}") int size
-    ) {
-        log.info("GET /admin/events?users={}&states={}&categories={}&rangeStart={}&rangeEnd={}&from={}&size={}",
-                users, states, categories, rangeStart, rangeEnd, from, size);
+//            @RequestParam(required = false) List<Long> users,
+//            @RequestParam(required = false) List<EventState> states,
+//            @RequestParam(required = false) List<Long> categories,
+//            @RequestParam(required = false) LocalDateTime rangeStart,
+//            @RequestParam(required = false) LocalDateTime rangeEnd,
+//            @RequestParam(required = false, defaultValue = "0") @Valid
+//            @Min(value = 0, message = "Параметр from не может быть меньше {value}") int from,
+//            @RequestParam(required = false, defaultValue = "10")
+//            @Min(value = 1, message = "Параметр size не может быть меньше {value}") @Valid
+//            @Max(value = 1000, message = "Параметр size не может быть больше {value}") int size
 
-        var params = AdminEventService.GetEventsParams.builder()
-                .users(users)
-                .states(states)
-                .categories(categories)
-                .rangeStart(rangeStart)
-                .rangeEnd(rangeEnd)
-                .from(from)
-                .size(size)
-                .build();
+            @ModelAttribute @Valid AdminEventService.GetEventsParams params
+    ) {
+        log.info("Получен запрос: GET /admin/events?users={}&states={}&categories={}&rangeStart={}&rangeEnd={}&from={}&size={}",
+                params.getUsers(), params.getStates(), params.getCategories(), params.getRangeStart(), params.getRangeEnd(), params.getFrom(), params.getSize());
+//        log.info("GET /admin/events?users={}&states={}&categories={}&rangeStart={}&rangeEnd={}&from={}&size={}",
+//                users, states, categories, rangeStart, rangeEnd, from, size);
+
+//        var params = AdminEventService.GetEventsParams.builder()
+//                .users(users)
+//                .states(states)
+//                .categories(categories)
+//                .rangeStart(rangeStart)
+//                .rangeEnd(rangeEnd)
+//                .from(from)
+//                .size(size)
+//                .build();
 
         return ResponseEntity
                 .ok()
