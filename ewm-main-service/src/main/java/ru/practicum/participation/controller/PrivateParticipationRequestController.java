df88e5a76450e6ab6a73f36c230f113bfd542e04
Nikolay <Novizdarialokin@yandex.ru>
2024-07-04 22:28:22 +0600

    fix: Убрал комментарии

--- ewm-main-service/src/main/java/ru/practicum/participation/controller/PrivateParticipationRequestController.java
+++ ewm-main-service/src/main/java/ru/practicum/participation/controller/PrivateParticipationRequestController.java
@@ -20,53 +20,32 @@ public class PrivateParticipationRequestController {
     private final PrivateParticipationRequestService privateParticipationRequestService;
 
     @GetMapping
-    public ResponseEntity<Object> getParticipationRequests(
-            @PathVariable Long userId
-    ) {
-        log.info("GET /users/{}/requests", userId);
+    public ResponseEntity<Object> getParticipationRequests(@PathVariable Long userId) {
+        log.info("Получен запрос: GET /users/{}/requests", userId);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(privateParticipationRequestService.getParticipationRequests(userId));
-
-        //todo В случае, если по заданным фильтрам не найдено ни одной заявки, возвращает пустой список
-        //  400 если запрос составлен некоректно
-        //  404 если пользователь не найден
     }
 
     @PostMapping
-    public ResponseEntity<Object> addParticipationRequest(
-            @PathVariable Long userId,
-            @RequestParam Long eventId
-    ) {
-        log.info("POST /users/{}/requests?eventId={}", userId, eventId);
+    public ResponseEntity<Object> addParticipationRequest(@PathVariable Long userId, @RequestParam Long eventId) {
+        log.info("Получен запрос: POST /users/{}/requests?eventId={}", userId, eventId);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(privateParticipationRequestService.addParticipationRequest(userId, eventId));
-
-        //todo Обратите внимание:
-        // нельзя добавить повторный запрос (Ожидается код ошибки 409)
-        // инициатор события не может добавить запрос на участие в своём событии (Ожидается код ошибки 409)
-        // нельзя участвовать в неопубликованном событии (Ожидается код ошибки 409)
-        // если у события достигнут лимит запросов на участие - необходимо вернуть ошибку (Ожидается код ошибки 409)
-        // если для события отключена пре-модерация запросов на участие, то запрос должен автоматически перейти в состояние подтвержденного
     }
 
     @PatchMapping("{requestId}/cancel")
-    public ResponseEntity<Object> cancelParticipationRequest(
-            @PathVariable Long userId,
-            @PathVariable Long requestId
-    ) {
-        log.info("PATCH /users/{}/requests/{}/cancel", userId, requestId);
+    public ResponseEntity<Object> cancelParticipationRequest(@PathVariable Long userId, @PathVariable Long requestId) {
+        log.info("Получен запрос: PATCH /users/{}/requests/{}/cancel", userId, requestId);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(privateParticipationRequestService.cancelParticipationRequest(userId, requestId));
-
-        //todo
     }
 }
