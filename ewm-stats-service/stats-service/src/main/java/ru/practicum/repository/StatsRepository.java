0ba6bff504fc1e04bf3e1db63859fea59d7cef04
Nikolay <Novizdarialokin@yandex.ru>
2024-07-04 12:22:10 +0600

    fix:

--- ewm-stats-service/stats-service/src/main/java/ru/practicum/repository/StatsRepository.java
+++ ewm-stats-service/stats-service/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -1,12 +1,7 @@
 package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.model.Hit;
-
-import java.time.ZonedDateTime;
-import java.util.List;
 
 /**
  * @author Nikolay Radzivon
