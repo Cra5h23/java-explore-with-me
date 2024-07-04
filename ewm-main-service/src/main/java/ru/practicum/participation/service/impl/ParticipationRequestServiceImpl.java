b9a0df305dfd996c0b507a590e2bd4616daa48af
Nikolay <Novizdarialokin@yandex.ru>
2024-07-04 22:53:08 +0600

    feat: логирование

--- ewm-main-service/src/main/java/ru/practicum/participation/service/impl/ParticipationRequestServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/participation/service/impl/ParticipationRequestServiceImpl.java
@@ -1,12 +1,14 @@
 package ru.practicum.participation.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.event.dto.EventRequestStatus;
 import ru.practicum.event.dto.EventRequestStatusUpdateRequest;
 import ru.practicum.event.dto.EventRequestStatusUpdateResult;
-import ru.practicum.participation.dto.ParticipationRequestDto;
 import ru.practicum.exception.NotFoundParticipationRequestException;
+import ru.practicum.participation.dto.ParticipationRequestDto;
 import ru.practicum.participation.mapper.ParticipationRequestMapper;
 import ru.practicum.participation.model.ParticipationRequest;
 import ru.practicum.participation.repository.ParticipationRequestRepository;
@@ -21,12 +23,15 @@ import java.util.stream.Collectors;
  */
 @Service
 @RequiredArgsConstructor
+@Transactional
+@Slf4j
 public class ParticipationRequestServiceImpl implements ParticipationRequestService {
     private final ParticipationRequestRepository participationRequestRepository;
     private final ParticipationRequestMapper participationRequestMapper;
 
     @Override
     public ParticipationRequest checkParticipationRequest(Long partId) {
+        log.info("Проверка на то что запрос на участие с id {} существует", partId);
         return participationRequestRepository.findById(partId)
                 .orElseThrow(() -> new NotFoundParticipationRequestException(
                         String.format("Запрос с id %d не найден или не доступен", partId)));
@@ -34,6 +39,7 @@ public class ParticipationRequestServiceImpl implements ParticipationRequestServ
 
     @Override
     public ParticipationRequest checkParticipationRequest(Long partId, Long userId) {
+        log.info("Проверка на то что запрос на участие с id {} существует у пользователя с id {}", partId, userId);
         return participationRequestRepository.findByIdAndRequesterId(partId, userId)
                 .orElseThrow(() -> new NotFoundParticipationRequestException(
                         String.format("Запрос с id %d у пользователя с id %d не найден или недоступен", partId, userId)));
@@ -41,6 +47,7 @@ public class ParticipationRequestServiceImpl implements ParticipationRequestServ
 
     @Override
     public List<ParticipationRequestDto> getEventRequests(Long userId, Long eventId) {
+        log.info("Получение списка запросов на участие для события с id {} и инициатора {}", eventId, userId);
         return participationRequestRepository.findAllByEventInitiatorIdAndEventId(userId, eventId)
                 .stream()
                 .map(participationRequestMapper::toParticipationRequestDto)
@@ -48,8 +55,11 @@ public class ParticipationRequestServiceImpl implements ParticipationRequestServ
     }
 
     @Override
-    public EventRequestStatusUpdateResult confirmUserRequests(Long userId, Long eventId, EventRequestStatusUpdateRequest request) {
-        participationRequestRepository.updateStatusForRequests(request.getStatus(), request.getRequestIds(), eventId, userId);
+    public EventRequestStatusUpdateResult confirmUserRequests(Long userId, Long eventId,
+                                                              EventRequestStatusUpdateRequest request) {
+        log.info("Подтверждение запросов на участие для события с id {} и инициатора с id {}, {}", eventId, userId, request);
+        participationRequestRepository.updateStatusForRequests(request.getStatus(), request.getRequestIds(), eventId,
+                userId);
         var eventRequests = getEventRequests(userId, eventId);
         var confirmed = eventRequests.stream()
                 .filter(r -> r.getStatus().equals(EventRequestStatus.CONFIRMED))
