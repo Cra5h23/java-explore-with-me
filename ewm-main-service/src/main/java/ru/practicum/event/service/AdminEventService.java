045b1af10474616d5b4f86ad87848a2762e7e75b
Nikolay <Novizdarialokin@yandex.ru>
2024-07-04 20:50:39 +0600

    feat: Добавил валидацию

--- ewm-main-service/src/main/java/ru/practicum/event/service/AdminEventService.java
+++ ewm-main-service/src/main/java/ru/practicum/event/service/AdminEventService.java
@@ -1,11 +1,16 @@
 package ru.practicum.event.service;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.event.dto.EventFullDtoResponse;
 import ru.practicum.event.dto.EventState;
 import ru.practicum.event.dto.UpdateEventAdminRequest;
+import ru.practicum.validator.ValidDateRange;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -20,13 +25,21 @@ public interface AdminEventService {
 
     @Builder
     @Data
+    @ValidDateRange
+    @NoArgsConstructor
+    @AllArgsConstructor
     class GetEventsParams {
         private List<Long> users;
         private List<EventState> states;
         private List<Long> categories;
         private LocalDateTime rangeStart;
         private LocalDateTime rangeEnd;
-        private int from;
-        private int size;
+        @Min(value = 0, message = "Параметр from не может быть меньше {value}")
+        @Builder.Default
+        private int from = 0;
+        @Min(value = 1, message = "Параметр size не может быть меньше {value}")
+        @Max(value = 1000, message = "Параметр size не может быть больше {value}")
+        @Builder.Default
+        private int size = 10;
     }
 }
