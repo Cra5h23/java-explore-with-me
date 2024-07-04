68d42c7ba957de05981edd40add9f6053b4d78bc
Nikolay <Novizdarialokin@yandex.ru>
2024-07-04 20:48:28 +0600

    refactor: Переименовал DateRangeValidator -> PublicGetEventParamsDateRangeValidator

--- ewm-main-service/src/main/java/ru/practicum/validator/PublicGetEventParamsDateRangeValidator.java
+++ ewm-main-service/src/main/java/ru/practicum/validator/PublicGetEventParamsDateRangeValidator.java
@@ -9,7 +9,7 @@ import javax.validation.ConstraintValidatorContext;
  * @author Nikolay Radzivon
  * @Date 02.07.2024
  */
-class DateRangeValidator implements ConstraintValidator<ValidDateRange, PublicEventService.GetEventsParams> {
+class PublicGetEventParamsDateRangeValidator implements ConstraintValidator<ValidDateRange, PublicEventService.GetEventsParams> {
 
     @Override
     public boolean isValid(PublicEventService.GetEventsParams eventsParams, ConstraintValidatorContext context) {
