63b894ed5d57900a2b51c5d1e484c133c5d99d17
Nikolay <Novizdarialokin@yandex.ru>
2024-07-04 21:09:24 +0600

    fix: Убрал комментарии

--- ewm-main-service/src/main/java/ru/practicum/event/controller/PrivateEventController.java
+++ ewm-main-service/src/main/java/ru/practicum/event/controller/PrivateEventController.java
@@ -37,78 +37,55 @@ public class PrivateEventController {
             @Min(value = 1, message = "Параметр size не может быть меньше {value}")
             @Max(value = 1000, message = "Параметр size не может быть больше {value}") int size
     ) {
-        log.info("GET /users/{}/events?from={}&size={}", userId, from, size);
+        log.info("Получен запрос: GET /users/{}/events?from={}&size={}", userId, from, size);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(privateEventService.getUserEvents(userId, from, size));
     }
-    //todo Возвращает пустой список если ничего не найдено. Возвращает 400 код когда запрос создан неправильно
 
     @PostMapping
-    public ResponseEntity<Object> addEvent(
-            @PathVariable Long userId,
-            @RequestBody @Valid EventDtoRequest event
-    ) {
-        log.info("POST /users/{}/events body={}", userId, event);
+    public ResponseEntity<Object> addEvent(@PathVariable Long userId, @RequestBody @Valid EventDtoRequest event) {
+        log.info("Получен запрос: POST /users/{}/events body={}", userId, event);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(privateEventService.addEvent(userId, event));
-
-        //todo Возвращает 400 код когда запрос создан неправильно, 409 если не удовлетворяет правилам создания
     }
 
     @GetMapping("/{eventId}")
-    public ResponseEntity<Object> getUserEvent(
-            @PathVariable Long userId,
-            @PathVariable Long eventId
-    ) {
-        log.info("GET /users/{}/events/{}", userId, eventId);
+    public ResponseEntity<Object> getUserEvent(@PathVariable Long userId, @PathVariable Long eventId) {
+        log.info("Получен запрос: GET /users/{}/events/{}", userId, eventId);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(privateEventService.getUserEvent(userId, eventId));
-
-        //todo Возвращает 400 код если запрос создан некорректно, 404 если событие не найдено или недоступно
     }
 
     @PatchMapping("/{eventId}")
     public ResponseEntity<Object> updateUserEvent(
             @PathVariable Long userId,
             @PathVariable Long eventId,
-            @RequestBody @Validated() UpdateEventUserRequest event
+            @RequestBody @Validated UpdateEventUserRequest event
     ) {
-        log.info("PATCH /users/{}/events/{} body={}", userId, eventId, event);
+        log.info("Получен запрос: PATCH /users/{}/events/{} body={}", userId, eventId, event);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(privateEventService.updateUserEvent(userId, eventId, event));
-
-//        todo изменить можно только отмененные события или события в состоянии ожидания модерации (Ожидается код ошибки 409)
-//         дата и время на которые намечено событие не может быть раньше, чем через два часа от текущего момента (Ожидается код ошибки 409)
-//         400 код если запрос составлен не корректно
-//         404 событие не найдено или недоступно
-//         409 событие не удовлетворяет правилам редактирования
-
     }
 
     @GetMapping("/{eventId}/requests")
-    public ResponseEntity<Object> getEventRequests(
-            @PathVariable Long userId,
-            @PathVariable Long eventId
-    ) {
-        log.info("GET /users/{}/events/{}/requests", userId, eventId);
+    public ResponseEntity<Object> getEventRequests(@PathVariable Long userId, @PathVariable Long eventId) {
+        log.info("Получен запрос: GET /users/{}/events/{}/requests", userId, eventId);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(privateEventService.getEventRequests(userId, eventId));
-
-        // todo 400 код когда запрос составлен некорректно. пустой список если ничего не найдено
     }
 
     @PatchMapping("/{eventId}/requests")
@@ -117,18 +94,11 @@ public class PrivateEventController {
             @PathVariable Long eventId,
             @RequestBody(required = false) EventRequestStatusUpdateRequest request
     ) {
-        log.info("PATCH /users/{}/events/{}/requests body={}", userId, eventId, request);
+        log.info("Получен запрос: PATCH /users/{}/events/{}/requests body={}", userId, eventId, request);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(privateEventService.confirmUserRequests(userId, eventId, request));
-
-
-        //todo Обратите внимание:
-        // если для события лимит заявок равен 0 или отключена пре-модерация заявок, то подтверждение заявок не требуется
-        // нельзя подтвердить заявку, если уже достигнут лимит по заявкам на данное событие (Ожидается код ошибки 409)
-        // статус можно изменить только у заявок, находящихся в состоянии ожидания (Ожидается код ошибки 409)
-        // если при подтверждении данной заявки, лимит заявок для события исчерпан, то все неподтверждённые заявки необходимо отклонить
     }
 }
