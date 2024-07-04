53eb7ca657bab7850322e8bb0c4431e518cca656
Nikolay <Novizdarialokin@yandex.ru>
2024-07-04 20:49:12 +0600

    feat: Добавил второй валидатор AdminGetEventParamsDataRangeValidator

--- ewm-main-service/src/main/java/ru/practicum/validator/ValidDateRange.java
+++ ewm-main-service/src/main/java/ru/practicum/validator/ValidDateRange.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = DateRangeValidator.class)
+@Constraint(validatedBy = {PublicGetEventParamsDateRangeValidator.class, AdminGetEventParamsDataRangeValidator.class})
 public @interface ValidDateRange {
     String message() default "Дата окончания поиска должна быть позже даты начала поиска";
 
