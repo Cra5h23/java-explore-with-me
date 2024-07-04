3f9c9a39c7aca50fc2843cc7a5f5f4d18e5b43e1
Nikolay <Novizdarialokin@yandex.ru>
2024-07-04 21:00:19 +0600

    fix: Убрал комментарии

--- ewm-main-service/src/main/java/ru/practicum/event/controller/AdminEventController.java
+++ ewm-main-service/src/main/java/ru/practicum/event/controller/AdminEventController.java
@@ -30,42 +30,15 @@ public class AdminEventController {
      * Метод для работы
      */
     @GetMapping
-    public ResponseEntity<Object> getEvents(
-//            @RequestParam(required = false) List<Long> users,
-//            @RequestParam(required = false) List<EventState> states,
-//            @RequestParam(required = false) List<Long> categories,
-//            @RequestParam(required = false) LocalDateTime rangeStart,
-//            @RequestParam(required = false) LocalDateTime rangeEnd,
-//            @RequestParam(required = false, defaultValue = "0") @Valid
-//            @Min(value = 0, message = "Параметр from не может быть меньше {value}") int from,
-//            @RequestParam(required = false, defaultValue = "10")
-//            @Min(value = 1, message = "Параметр size не может быть меньше {value}") @Valid
-//            @Max(value = 1000, message = "Параметр size не может быть больше {value}") int size
-
-            @ModelAttribute @Valid AdminEventService.GetEventsParams params
-    ) {
+    public ResponseEntity<Object> getEvents(@ModelAttribute @Valid AdminEventService.GetEventsParams params) {
         log.info("Получен запрос: GET /admin/events?users={}&states={}&categories={}&rangeStart={}&rangeEnd={}&from={}&size={}",
-                params.getUsers(), params.getStates(), params.getCategories(), params.getRangeStart(), params.getRangeEnd(), params.getFrom(), params.getSize());
-//        log.info("GET /admin/events?users={}&states={}&categories={}&rangeStart={}&rangeEnd={}&from={}&size={}",
-//                users, states, categories, rangeStart, rangeEnd, from, size);
-
-//        var params = AdminEventService.GetEventsParams.builder()
-//                .users(users)
-//                .states(states)
-//                .categories(categories)
-//                .rangeStart(rangeStart)
-//                .rangeEnd(rangeEnd)
-//                .from(from)
-//                .size(size)
-//                .build();
+                params.getUsers(), params.getStates(), params.getCategories(), params.getRangeStart(),
+                params.getRangeEnd(), params.getFrom(), params.getSize());
 
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(adminEventService.getEvents(params));
-
-        //todo Эндпоинт возвращает полную информацию обо всех событиях подходящих под переданные условия
-        //  В случае, если по заданным фильтрам не найдено ни одного события, возвращает пустой список
     }
 
     @PatchMapping("/{eventId}")
@@ -73,16 +46,11 @@ public class AdminEventController {
             @PathVariable Long eventId,
             @RequestBody @Valid UpdateEventAdminRequest event
     ) {
-        log.info("PATCH admin/events/{} body={}", eventId, event);
+        log.info("Получен запрос: PATCH admin/events/{} body={}", eventId, event);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(adminEventService.updateEvent(eventId, event));
-
-        //todo Редактирование данных любого события администратором. Валидация данных не требуется. Обратите внимание:
-        // дата начала изменяемого события должна быть не ранее чем за час от даты публикации. (Ожидается код ошибки 409)
-        // событие можно публиковать, только если оно в состоянии ожидания публикации (Ожидается код ошибки 409)
-        // событие можно отклонить, только если оно еще не опубликовано (Ожидается код ошибки 409)
     }
 }
