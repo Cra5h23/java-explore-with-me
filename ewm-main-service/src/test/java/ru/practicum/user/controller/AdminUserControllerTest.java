98eb21ce8af6aac92687ab2ff264d72d74c6a51f
Nikolay <Novizdarialokin@yandex.ru>
2024-07-05 02:23:03 +0600

    fix:

--- ewm-main-service/src/test/java/ru/practicum/user/controller/AdminUserControllerTest.java
+++ ewm-main-service/src/test/java/ru/practicum/user/controller/AdminUserControllerTest.java
@@ -8,12 +8,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.practicum.exception.NotFoundUserException;
 import ru.practicum.user.dto.NewUserRequest;
 import ru.practicum.user.dto.UserDtoResponse;
-import ru.practicum.exception.NotFoundUserException;
 import ru.practicum.user.service.AdminUserService;
 
 import java.util.List;
