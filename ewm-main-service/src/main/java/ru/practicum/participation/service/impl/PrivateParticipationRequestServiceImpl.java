77db314e8b6ff356a50bb521e2d81abcf14c9291
Nikolay <Novizdarialokin@yandex.ru>
2024-07-06 20:34:30 +0600

    fix: Транзакции

--- ewm-main-service/src/main/java/ru/practicum/participation/service/impl/PrivateParticipationRequestServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/participation/service/impl/PrivateParticipationRequestServiceImpl.java
@@ -3,7 +3,6 @@ package ru.practicum.participation.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.event.dto.EventRequestStatus;
 import ru.practicum.event.dto.EventState;
