877fd818441454718275840a9456cb75a8826f3c
Nikolay <Novizdarialokin@yandex.ru>
2024-07-10 21:30:04 +0600

    feat: Реализация

--- ewm-main-service/src/main/java/ru/practicum/location/mapper/LocationMapper.java
+++ ewm-main-service/src/main/java/ru/practicum/location/mapper/LocationMapper.java
@@ -2,15 +2,15 @@ package ru.practicum.location.mapper;
 
 import org.springframework.stereotype.Component;
 import ru.practicum.event.dto.EventShortDto;
-import ru.practicum.event.repository.EventRepository;
+import ru.practicum.location.dto.AdminLocationDtoCreated;
+import ru.practicum.location.dto.AdminLocationDtoRequest;
 import ru.practicum.location.dto.AdminLocationDtoResponse;
-import ru.practicum.location.dto.LocationDtoRequest;
-import ru.practicum.location.dto.LocationDtoResponse;
-import ru.practicum.location.model.AdminLocation;
+import ru.practicum.location.dto.UserLocationDtoResponse;
+import ru.practicum.location.model.Location;
+import ru.practicum.location.model.TypeLocation;
+import ru.practicum.location.repository.projection.LocationFullProjection;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * @author Nikolay Radzivon
@@ -18,38 +18,44 @@ import java.util.stream.Collectors;
  */
 @Component
 public class LocationMapper {
-    public AdminLocation toLocation(LocationDtoRequest dto) {
-        return AdminLocation.builder()
+    public Location toLocation(AdminLocationDtoRequest dto, TypeLocation typeLocation) {
+        return Location.builder()
                 .lat(dto.getLat())
                 .lon(dto.getLon())
                 .radius(dto.getRadius())
                 .description(dto.getDescription())
                 .name(dto.getName())
+                .type(typeLocation)
                 .build();
     }
 
-    public AdminLocationDtoResponse toAdminLocationDtoResponse(AdminLocation save) {
-        return AdminLocationDtoResponse.builder()
-                .id(save.getId())
-                .name(save.getName())
-                .description(save.getDescription())
-                .lat(save.getLat())
-                .lon(save.getLon())
-                .radius(save.getRadius())
+    public AdminLocationDtoCreated toAdminLocationDtoResponse(Location location) {
+        return AdminLocationDtoCreated.builder()
+                .id(location.getId())
+                .name(location.getName())
+                .description(location.getDescription())
+                .lat(location.getLat())
+                .lon(location.getLon())
+                .radius(location.getRadius())
                 .build();
     }
 
-    public List<LocationDtoResponse> toListLocationDtoResponse(List<AdminLocation> locations, Map<Long, List<EventShortDto>> events) {
-        return locations.stream()
-                .map(l -> LocationDtoResponse.builder()
-                        .id(l.getId())
-                        .lat(l.getLat())
-                        .lon(l.getLon())
-                        .radius(l.getRadius())
-                        .name(l.getName())
-                        .description(l.getDescription())
-                        .events(events != null ? events.getOrDefault(l.getId(), List.of()) : List.of())
-                        .build()
-                ).collect(Collectors.toList());
+    public UserLocationDtoResponse toLocationDtoResponse(LocationFullProjection location, List<EventShortDto> events) {
+        return location.getType() == TypeLocation.ADMINS ?
+                AdminLocationDtoResponse.builder()
+                        .lon(location.getLon())
+                        .lat(location.getLat())
+                        .id(location.getId())
+                        .events(events)
+                        .radius(location.getRadius())
+                        .description(location.getDescription())
+                        .name(location.getName())
+                        .build() :
+                UserLocationDtoResponse.builder()
+                        .id(location.getId())
+                        .lat(location.getLat())
+                        .lon(location.getLon())
+                        .events(events)
+                        .build();
     }
 }
