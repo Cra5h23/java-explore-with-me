fe0e08c466a059e7959b9bc81f2fc128831f770c
Nikolay <Novizdarialokin@yandex.ru>
2024-07-10 21:05:57 +0600

    fix:

--- ewm-main-service/src/main/java/ru/practicum/location/service/AdminLocationService.java
+++ ewm-main-service/src/main/java/ru/practicum/location/service/AdminLocationService.java
@@ -1,18 +1,16 @@
 package ru.practicum.location.service;
 
-import ru.practicum.location.dto.AdminLocationDtoResponse;
-import ru.practicum.location.dto.LocationDtoRequest;
+import ru.practicum.location.dto.AdminLocationDtoCreated;
+import ru.practicum.location.dto.AdminLocationDtoRequest;
 
 /**
  * @author Nikolay Radzivon
  * @Date 07.07.2024
  */
 public interface AdminLocationService {
-    AdminLocationDtoResponse addLocation(LocationDtoRequest locationDto);
+    AdminLocationDtoCreated addLocation(AdminLocationDtoRequest locationDto);
 
-    Object getLocation(Long locId);
-
-    Object updateLocation(Long locId, LocationDtoRequest locationDto);
+    AdminLocationDtoCreated updateLocation(Long locId, AdminLocationDtoRequest locationDto);
 
     void deleteLocation(Long locId);
 }
