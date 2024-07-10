02a4e27c1d1421e11f2695bec6bfe6af98978896
Nikolay <Novizdarialokin@yandex.ru>
2024-07-10 21:11:38 +0600

    feat: Реализация методов

--- ewm-main-service/src/main/java/ru/practicum/event/repository/CustomEventRepositoryImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/event/repository/CustomEventRepositoryImpl.java
@@ -1,13 +1,18 @@
 package ru.practicum.event.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import ru.practicum.event.dto.EventState;
 import ru.practicum.event.model.Event;
 import ru.practicum.event.repository.projection.EventShortProjection;
+import ru.practicum.event.service.PublicEventService;
+import ru.practicum.location.dto.EventSortType;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
-import java.util.List;
 
 /**
  * @author Nikolay Radzivon
@@ -18,12 +23,20 @@ public class CustomEventRepositoryImpl implements CustomEventRepository {
     EntityManager entityManager;
 
     @Override
-    public List<EventShortProjection> findEventsWithinRadius(double lat, double lon, double radius) {
+    public Page<EventShortProjection> findEventsWithinRadius(PublicEventService.GetSearchParams params) {
+        Double lat = params.getLat();
+        Double lon = params.getLon();
+        Double radius = params.getRadius();
+        int size = params.getSize();
+        int from = params.getFrom();
+
+        PageRequest pageable = PageRequest.of(from / size, size);
+
+
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<EventShortProjection> cq = cb.createQuery(EventShortProjection.class);
         Root<Event> eventRoot = cq.from(Event.class);
 
-        // Создание функции расстояния
         Expression<Double> distance = cb.function(
                 "distance",
                 Double.class,
@@ -33,11 +46,22 @@ public class CustomEventRepositoryImpl implements CustomEventRepository {
                 eventRoot.get("location").get("lon")
         );
 
-        // Условие для расстояния и статуса события
         Predicate distancePredicate = cb.lessThanOrEqualTo(distance, radius);
         Predicate statusPredicate = cb.equal(eventRoot.get("state"), EventState.PUBLISHED);
 
-        // Выборка полей, соответствующих интерфейсу EventShort
+        EventSortType eventStatus = params.getEventStatus();
+        Predicate eventDate = cb.conjunction();
+        switch (eventStatus) {
+            case ALL:
+                break;
+            case PAST:
+                eventDate = cb.lessThan(eventRoot.get("eventDate"), cb.currentDate());
+                break;
+            case UPCOMING:
+                eventDate = cb.greaterThan(eventRoot.get("eventDate"), cb.currentDate());
+                break;
+        }
+
         cq.multiselect(
                         eventRoot.get("id").alias("id"),
                         eventRoot.get("title").alias("title"),
@@ -49,8 +73,17 @@ public class CustomEventRepositoryImpl implements CustomEventRepository {
                         eventRoot.get("publishedOn").alias("publishedOn"),
                         eventRoot.get("confirmedRequests").alias("confirmedRequests") // Прямое использование поля с аннотацией @Formula
                 )
-                .where(cb.and(distancePredicate, statusPredicate));
+                .where(cb.and(distancePredicate, statusPredicate, eventDate));
 
-        return entityManager.createQuery(cq).getResultList();
+        CriteriaQuery<Long> countQuery = cb.createQuery(Long.class);
+        countQuery.select(cb.count(countQuery.from(Event.class)))
+                .where(cb.and(distancePredicate, statusPredicate));
+        Long count = entityManager.createQuery(countQuery).getSingleResult();
+
+        TypedQuery<EventShortProjection> typedQuery = entityManager.createQuery(cq);
+        typedQuery.setFirstResult((int) pageable.getOffset());
+        typedQuery.setMaxResults(pageable.getPageSize());
+
+        return new PageImpl<>(typedQuery.getResultList(), pageable, count);
     }
 }
