feff189ce0706e007c3694ce4ce05faa1f5df392
Nikolay <Novizdarialokin@yandex.ru>
2024-07-10 21:57:31 +0600

    feat: Добавил StringToEventSortTypeConverter

---
+++ ewm-main-service/src/main/java/ru/practicum/converter/StringToEventSortTypeConverter.java
@@ -0,0 +1,22 @@
+package ru.practicum.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import ru.practicum.exception.StringToEventSortTypeConverterException;
+import ru.practicum.location.dto.EventSortType;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 10.07.2024
+ */
+public class StringToEventSortTypeConverter implements Converter<String, EventSortType> {
+
+    @Override
+    public EventSortType convert(String source) {
+        try {
+            return EventSortType.valueOf(source.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new StringToEventSortTypeConverterException("Неверное значение для типа сортировки событий: " + source);
+        }
+    }
+}
