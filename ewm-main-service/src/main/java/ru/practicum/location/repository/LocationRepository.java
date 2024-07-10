77a48846fc283ff3486256404e5d5bca44ec0fc0
Nikolay <Novizdarialokin@yandex.ru>
2024-07-10 21:30:20 +0600

    feat: Реализация

--- ewm-main-service/src/main/java/ru/practicum/location/repository/LocationRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/location/repository/LocationRepository.java
@@ -1,17 +1,11 @@
 package ru.practicum.location.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import ru.practicum.location.dto.LocationDtoResponse;
-import ru.practicum.location.model.AdminLocation;
-
-import java.util.List;
+import ru.practicum.location.model.Location;
 
 /**
  * @author Nikolay Radzivon
  * @Date 07.07.2024
  */
-public interface LocationRepository extends JpaRepository<AdminLocation, Long> {
+public interface LocationRepository extends JpaRepository<Location, Long>, CustomLocationRepository {
 }
